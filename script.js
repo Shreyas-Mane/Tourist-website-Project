e4227cb00f93c0c3148632bef38cb75025174cc0
Shreyas-Mane <shreyas12mane@gmail.com>
2024-04-02 14:47:02 +0530

    second commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function checkDetail(){
-     dest = document.getElementById('goto').value;
-     date = document.getElementById('when').value;
-     option = document.getElementById('type').value;
+     var dest = document.getElementById('goto').value;
+     var date = document.getElementById('when').value;
+     var option = document.getElementById('type').value;
      
-    if(dest=="")
+    if(dest==null && date==null && option==null)
     {
         alert("Please enter your Destination,Date and Tour Type")
     }
@@ -21,5 +21,19 @@ function validate(){
     else{
         alert("your Email has been submitted..!")
     }
+
+}
+
+
+function feedback(){
+   name = document.getElementById('name').value;
+   email =document.getElementById('email').value;
+ text=document.getElementById('subject').value;
+ 
+  if(name==null && email==null && text==null){
+    alert("please enter the details below")
+  }
+  else
+  alert("your Feedback has been submitted..!")
 
 }
